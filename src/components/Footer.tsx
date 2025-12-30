f5df1361bf76ad0367f5921930cc1bbdbdd7fed4
Taiwo <taiwoaa04@gmail.com>
2025-12-30 12:59:40 +0100

    new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <section className="pt-6 flex flex-col items-center ">
+    <section className="pt-6 flex flex-col items-center overflow-hidden ">
         <hr className="text-white/40 w-[95%]"/>
         <div className="py-4">
             <span className="text-white/40">© 2025 Sarah Chen. All rights reserved. Published by Literary Press International.</span>
--- src/components/NavBottom.tsx
+++ src/components/NavBottom.tsx
@@ -21,7 +21,7 @@ const NavBottom = () => {
         {text:"FAQs", href:"#"},
     ]
   return (
-    <section id='footer' className=" py-6 bg-slate-900">
+    <section id='footer' className=" py-6 bg-slate-900 overflow-hidden">
         <div className="container max-w-7xl">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-11 lg:gap-4">
                 <div className="col-span-2 flex flex-col gap-7">
